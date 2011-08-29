a9e92e6374cc888f33dd8907b5035211c2ac9056
Eugene Sandulenko <sev@scummvm.org>
2011-08-30 00:33:01 +0200

    KOATUU: Filter out only place nodes

--- dumpcities.pl
+++ dumpcities.pl
@@ -15,10 +15,12 @@ BEGIN { $| = 1; }
 my $addname = sub {
 	my $place = $_[0]->{tag}->{place};
 
-	if ($place eq 'city' || $place eq 'town' || $place eq 'village' || $place eq 'hamlet') {
-		$_[0]->{action} = 'modify';
+	if ($_[0]->{type} eq 'node') {
+		if ($place eq 'city' || $place eq 'town' || $place eq 'village' || $place eq 'hamlet') {
+			$_[0]->{action} = 'modify';
 
-		print Geo::Parse::OSM->to_xml($_[0]);
+			print Geo::Parse::OSM->to_xml($_[0]);
+		}
 	}
 };
 
