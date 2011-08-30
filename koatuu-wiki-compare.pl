fd1bcbacba83d249c3ab6952788c44896a47f807
Eugene Sandulenko <sev@scummvm.org>
2011-08-30 02:28:49 +0200

    KOATUU: Build tree out of KOATUU codes

--- koatuu-wiki-compare.pl
+++ koatuu-wiki-compare.pl
@@ -14,6 +14,7 @@
 
 use utf8;
 use Text::CSV;
+use Data::Dumper;
 
 BEGIN { $| = 1; }
 
@@ -30,6 +31,10 @@ open my $fh, "<:encoding(utf8)", $koatuuname or die "$koatuuname: $!";
 $csv->column_names($csv->getline($fh));
 
 my %koatuus = ();
+%koatuutree = ();
+
+sub splitkoatuu($);
+sub buildkoatuutree();
 
 while (my $line = $csv->getline_hr($fh)) {
 	$line->{refs} = 0;
@@ -42,6 +47,12 @@ close $fh;
 undef $csv;
 
 print "Loaded ". (scalar keys %koatuus) . " KOATUUs from file $koatuuname\n";
+
+buildkoatuutree();
+
+print Dumper(\%koatuutree);
+
+exit 0;
 
 my @cities = ();
 
@@ -59,7 +70,7 @@ while (my $line = $csv->getline_hr($fh)) {
 	$line->{koatuu} =~ s/^\s+|\s+$//g;
 	$line->{name_ua} =~ s/́//g;
 	if ($line->{koatuu} =~ /^\d+$/) {
-	    $line->{koatuu} = sprintf("%010.0f", $line->{koatuu});
+		$line->{koatuu} = sprintf("%010.0f", $line->{koatuu});
 	}
 
 	push @cities, $line;
@@ -79,9 +90,9 @@ $matches = 0;
 
 for $c (@cities) {
 	if ($c->{koatuu} eq "") {
-	    #print "No KOATUU: $c->{name_ua}\n";
-	    $nokoatuu++;
-	    next;
+		#print "No KOATUU: $c->{name_ua}\n";
+		$nokoatuu++;
+		next;
 	}
 
 	if (not exists $koatuus{$c->{koatuu}}) {
@@ -128,3 +139,43 @@ for $k (sort keys %koatuus) {
 }
 
 print "Matches: $matches, Unknown: $unk, Mismatch: $mismatch, Missing: $missing, No KOATUU: $nokoatuu, Dup: $dup\n";
+
+exit 0;
+
+sub splitkoatuu($) {
+	my $koatuu = shift;
+
+	my $k = sprintf ("%010.0f", $koatuu);
+
+	my $l1 = substr($k, 0, 2);
+	my $l2 = substr($k, 2, 3);
+	my $l3 = substr($k, 5, 3);
+	my $l4 = substr($k, 8, 2);
+
+	return ($l1, $l2, $l3, $l4);
+}
+
+sub buildkoatuutree() {
+	my ($l1, $l2, $l3, $l4);
+
+	for $k (sort keys %koatuus) {
+		($l1, $l2, $l3, $l4) = splitkoatuu($k);
+
+		$koatuutree{$l1}{$l2}{$l3}{$l4} = $koatuus{$k};
+	}
+
+	# Calculate sizes at each level
+	$koatuutree{'size'} = scalar keys %koatuutree;
+
+	for $k1 (keys %koatuutree) {
+		$koatuutree{$k1}{'size'} = scalar keys %{ $koatuutree{$k1} };
+
+		for $k2 (keys %{ $koatuutree{$k1} }) {
+			$koatuutree{$k1}{$k2}{'size'} = scalar keys %{ $koatuutree{$k1}{$k2} };
+		
+			for $k3 (keys %{ $koatuutree{$k1}{$k2} }) {
+				$koatuutree{$k1}{$k2}{$k3}{'size'} = scalar keys %{ $koatuutree{$k1}{$k2}{$k3} };
+			}
+		}
+	}
+}
