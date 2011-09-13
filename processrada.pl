ee8f727d205fe1ce75783fd3538862504a3588bb
Eugene Sandulenko <sev@scummvm.org>
2011-09-13 12:28:49 +0100

    RADA: Extract more information from rada dump

--- processrada.pl
+++ processrada.pl
@@ -22,8 +22,9 @@ $csv->eol("\n");
 open $csvf, ">:encoding(utf8)", $outfile or die "$outfile: $!";
 
 my @fields1 = qw(oldname formdate categorydate zipname zip admcenter water areaga aream areakm populationt populationm populations density adminvalue distanceto distancerail distanceroad nearestrail distancenearestrail populationcity populationmiskradat populationmiskradam populationmiskradas);
-my @fields2 = qw(amts phone fax rada email site);
-my @fields3 = qw(numsil numselrad nummist nummiskrad numsel numsilrad numsel dummy numrayonsmist numrayradmist numrayons nummistobl numrayrad nummistresp);
+my @fields2 = qw(amts phone fax rada email site type parentname parentref);
+my @fieldsp = qw(radapriynyala priynaladate priynalanum priynyalazmist priynalavru priynyalapubdate priynyalapubstor priynyalatype radapriynyala2 priynaladate2 priynalanum2 priynyalazmist2 priynalavru2 priynyalapubdate2 priynyalapubstor2 priynyalatype2);
+my @fields3 = qw(numsil numsilw numselrad numselradw nummistray nummistrayw nummiskrad nummiskradw numsel numselw numsilrad numsilradw numsel numselw dummy dummyw numrayonsmist numrayonmistw numrayradmist numrayradmistw numrayons numrayonsw nummistobl nummistoblw numrayrad numrayradw nummistresp nummistrespw);
 
 print $csvf "\"num\",\"name\"";
 
@@ -35,12 +36,22 @@ for my $f (@fields2) {
 	print $csvf ",\"$f\"";
 }
 
+for my $f (@fieldsp) {
+	print $csvf ",\"$f\"";
+}
+
+for my $f (@fields3) {
+	print $csvf ",\"$f\"";
+}
+
 print $csvf "\n";
 
 for my $f (<${radadir}/*.html>) {
 	my %entry = ();
 	my $fields1num = -1;
 	my $fields2num = -1;
+	my $fieldspnum = -1;
+	my $fieldspcoeff = 0;
 	my $fields3num = -1;
 
 	print "\r$f";
@@ -51,11 +62,27 @@ for my $f (<${radadir}/*.html>) {
 
 	$entry{num} = $1;
 
+	my $fields2val = "";
+	my $fields2priynyala = 0;
+	my $inpriynyala = 0;
+
 	while (<$in>) {
-		next unless /class="AllNews"/;
+		if ($fields2priynyala == 2 and $fieldspnum == 4 and $inpriynyala) {
+			if (/<\/td>/) {
+				$inpriynyala = 0;
+				next;
+			}
+			chomp;
+			$entry{$fieldsp[$fieldspnum - 1 + 8 * $fieldspcoeff]} .= " $_";
+			next;
+		}
+
+		next unless /class="AllNews"/ or /class="topTitle"/;
 
 		if (/h3 align=center.*AllNews">(.*)<\/h3>/) {
 			$entry{name} = $1;
+		} elsif (/THEAD3.*align=right\s+width="2%".*AllNews">(.*)<\/td>/) {
+			# Just skip it
 		} elsif (/THEAD3.*AllNews">(.*)<\/td>/) {
 			if ($1 =~ /^Колишня назва/) {
 				$fields1num = 0;
@@ -105,43 +132,52 @@ for my $f (<${radadir}/*.html>) {
 				$fields1num = 22;
 			} elsif ($1 =~ /^Населення \(міськрада\) сільське \(тис.осіб/) {
 				$fields1num = 23;
+			} else {
+				die "$f: Unknown field: $1 <$_> at $.";
 			}
 		} elsif (/THEAD21.*AllNews">(.*)<\/td>/) {
 			if ($fields1num == -1) {
+				my $p = $fields3num;
+
 				if ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;сіл&nbsp;/) {
 					$fields3num = 0;
 				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;селищних рад&nbsp;/) {
-					$fields3num = 1;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;міст районного значення&nbsp;/) {
 					$fields3num = 2;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;міських рад&nbsp;/) {
-					$fields3num = 3;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;селищ міського типу&nbsp;/) {
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;міст районного значення&nbsp;/) {
 					$fields3num = 4;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;сільських рад&nbsp;/) {
-					$fields3num = 5;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;селищ&nbsp;/) {
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;міських рад&nbsp;/) {
 					$fields3num = 6;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/) {
-					$fields3num = 7;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районів у містах&nbsp;/) {
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;селищ міського типу&nbsp;/) {
 					$fields3num = 8;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районних рад у містах&nbsp;/) {
-					$fields3num = 9;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районів&nbsp;/) {
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;сільських рад&nbsp;/) {
 					$fields3num = 10;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;міст обласного значення&nbsp;/) {
-					$fields3num = 11;
-				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районних рад&nbsp;/) {
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;селищ&nbsp;/) {
 					$fields3num = 12;
-				} elsif ($1 =~ /^/) {
-					$fields3num = 13;
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$/) {
+					$fields3num = 14;
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районів у містах&nbsp;/) {
+					$fields3num = 16;
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районних рад у містах&nbsp;/) {
+					$fields3num = 18;
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районів&nbsp;/) {
+					$fields3num = 20;
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;міст обласного значення&nbsp;/) {
+					$fields3num = 22;
+				} elsif ($1 =~ /^&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;районних рад&nbsp;/) {
+					$fields3num = 24;
 				} else {
-					die "$f: $_";
+					die "$f:(3) $_  at $.";
 				}
+
+				if ($fields3num <= $p) {
+					die "$f: Wrong field seq: $_  at $.";
+				}
+				$fields2priynyala = 0;
 			}
 
 			if ($fields3num == -1) {
+				die "$f: Bad field composition: $_ at $." if $fields1num == -1;
+
 				my $res = $1;
 				$res =~ s/&nbsp;/ /g;
 				$res =~ s/^\s+|\s+$//g;
@@ -150,63 +186,209 @@ for my $f (<${radadir}/*.html>) {
 
 				$fields1num = -1;
 			}
-		} elsif (/THEAD21.*topTitle">.*">(.*)<\/a>/) {
+		} elsif (/THEAD21.*topTitle"><a href="(.*)">(.*)<\/a>/) {
 			if ($fields3num == -1) {
-				die "$f: $_";
+				die "$f: unknown thead21 $_ at $.";
 			}
 
 			my $res = $1;
+			my $res2 = $2;
 			$res =~ s/&nbsp;/ /g;
 			$res =~ s/^\s+|\s+$//g;
+			$res2 =~ s/&nbsp;/ /g;
+			$res2 =~ s/^\s+|\s+$//g;
 
 			$entry{$fields3[$fields3num]} = $res;
+			$entry{$fields3[$fields3num+1]} = $res2;
 
 			$fields3num = -1;
+		} elsif (/THEAD21.*topTitle">/) {
+			die "$f: Bad format: $_ at $." unless $fields3num == 14;
 		} elsif (/THEAD00.*AllNews">(.*)<\/td>/) {
 			if ($1 =~ /^Код АМТС/) {
-				die "$f: $_" unless $fields2num == -1;
+				die "$f: bad thead00 seq ($fields2num) $_ at $." unless ($fields2num == 6 or $fields2num == -1);
 				$fields2num = 0;
 			} elsif ($1 =~ /^Телефон/) {
-				die "$f: $_" unless $fields2num == 0;
+				die "$f: bad thead00 seq $_ at $." unless $fields2num == 0;
 				$fields2num = 1;
 			} elsif ($1 =~ /^Факс/) {
-				die "$f: $_" unless $fields2num == 1;
+				die "$f: bad thead00 seq $_ at $." unless $fields2num == 1;
 				$fields2num = 2;
 			} elsif ($1 =~ /^Адреса ради/) {
-				die "$f: $_" unless $fields2num == 2;
+				die "$f: bad thead00 seq $_ at $." unless $fields2num == 2;
 				$fields2num = 3;
 			} elsif ($1 =~ /^E-mail/) {
-				die "$f: $_" unless $fields2num == 3;
+				die "$f: bad thead00 seq $_ at $." unless $fields2num == 3;
 				$fields2num = 4;
 			} elsif ($1 =~ /^Адреса сайту/) {
-				die "$f: $_" unless $fields2num == 4;
+				die "$f: bad thead00 seq $_ at $." unless $fields2num == 4;
 				$fields2num = 0;
-			} elsif ($1 =~ /^Рада, що прийняла/) {
-				last;
 			} elsif ($1 =~ /^У складі району:/) {
-				$fields2num = -1;
+				die "$f: doubled type: $_ at $." if $fields2val ne "";
+
+				$fields2num = 6;
+				$fields2val = "rayon";
 			} elsif ($1 =~ /^У системі місцевого самоврядування:/) {
-				$fields2num = -1;
 			} elsif ($1 =~ /^У складі міста:/) {
-				$fields2num = -1;
+				die "$f: doubled type: $_ at $." if $fields2val ne "";
+				$fields2num = 6;
+				$fields2val = "misto";
 			} elsif ($1 =~ /^У складі області:/) {
-				$fields2num = -1;
+				die "$f: doubled type: $_ at $." if $fields2val ne "";
+				$fields2num = 6;
+				$fields2val = "oblast";
 			} elsif ($1 =~ /^У складі автономії:/) {
-				$fields2num = -1;
+				die "$f: doubled type: $_ at $." if $fields2val ne "";
+				$fields2num = 6;
+				$fields2val = "avtonomia";
+			} elsif ($1 =~ /^Рада, що прийняла<br>рішення/) {
+				die "$f: Priynyala: $_ at $." if $fields2priynyala;
+
+				$fields2priynyala = 1;
+				$fieldspnum = 0;
+			} elsif ($1 =~ /^Дата<br>рішення/) {
+				die "$f: Priynyala: $_ at $." if $fields2priynyala != 1 or $fieldspnum != 0;
+
+				$fieldspnum = 1;
+			} elsif ($1 =~ /^№<br>рішення/) {
+				die "$f: Priynyala: $_ at $." if $fields2priynyala != 1 or $fieldspnum != 1;
+
+				$fieldspnum = 2;
+			} elsif ($1 =~ /^Зміст рішення/) {
+				die "$f: Priynyala: $_ at $." if $fields2priynyala != 1 or $fieldspnum != 2;
+
+				$fieldspnum = 3;
+			} elsif ($1 =~ /^Відомості ВРУ/) {
+				die "$f: Priynyala: $_ at $." if $fields2priynyala != 1 or $fieldspnum != 3;
+
+				$fieldspnum = 4;
 			} else {
-				die "$f: $_";
+				die "$f: Bad typedef $_ at $.";
 			}
-		} elsif (/THEAD02.*AllNews">(.*)<\/td>/) {
-			if ($fields2num > 5 || $fields2num == -1) {
-				die "$f: $_";
+		} elsif (/THEAD01.*AllNews">(.*)<\/td>/) {
+			if ($fields2priynyala < 1 and $fieldspcoeff == 0) {
+				die "$f: Unexpected thead01: $_ at $.";
+			} elsif ($1 =~ /^Дата<br>опубліку-<br>вання/) {
+				die "$f: Priynyala: $_ ($fieldspnum $fieldspcoeff) at $." if $fieldspnum != 4 and $fieldspcoeff == 0;
+
+				$fieldspnum = 5;
+				$fields2priynyala = 1;
+			} elsif ($1 =~ /^№, стаття<br>\(сторінка\)/) {
+				die "$f: Priynyala: $_ at $." if $fieldspnum != 5;
+
+				$fieldspnum = 6;
+				$fields2priynyala = 2;
+			} else {
+				die "$f: Priynyala: $_ ($1) at $.";
+			}				
+		} elsif (/THEAD02.*AllNews">(.*)<\/td>/ or ($fieldspnum == 3 and /THEAD02.*AllNews">(.*)/)) {
+			if ($fields2priynyala == 0) {
+				if ($fields2num > 6 || $fields2num == -1) {
+					die "$f: bad thead02 num $_ at $.";
+				}
+
+				my $res = $1;
+
+				$res = $fields2val if $fields2num == 6;
+
+				$res =~ s/&nbsp;/ /g;
+				$res =~ s/^\s+|\s+$//g;
+
+				$entry{$fields2[$fields2num]} = $res;
+				$fields2num++;
+			} else {
+				if ($fieldspnum == 6) {
+					if (/Зміна категорії \(статусу\) населеного пункту/) {
+						$entry{$fieldsp[7 + 8 * $fieldspcoeff]} = "StatusChange";
+						$fieldspnum = 0;
+						next;
+					} elsif (/Перейменування/) {
+						$entry{$fieldsp[7 + 8 * $fieldspcoeff]} = "Rename";
+						$fieldspnum = 0;
+						next;
+					} elsif (/Ліквідація АТО \(зняття з обліку, об'єднання\)/) {
+						$entry{$fieldsp[7 + 8 * $fieldspcoeff]} = "Delete";
+						$fieldspnum = 0;
+						next;
+					} elsif (/Зміна центра АТО/) {
+						$entry{$fieldsp[7 + 8 * $fieldspcoeff]} = "CentreChange";
+						$fieldspnum = 0;
+						next;
+					} elsif (/Перепідпорядкування/) {
+						$entry{$fieldsp[7 + 8 * $fieldspcoeff]} = "ChangeSuper";
+						$fieldspnum = 0;
+						next;
+					} elsif (/Зміна меж АТО/) {
+						$entry{$fieldsp[7 + 8 * $fieldspcoeff]} = "BorderChange";
+						$fieldspnum = 0;
+						next;
+					} else {
+						die "$f: Bad priynyalatype: $_ at $.";
+					}
+				} elsif ($fieldspnum > 5 || $fieldspnum < 0) {
+					die "$f: bad thead02 num ($fieldspnum) $_ at $.";
+				}
+				my $res = $1;
+
+				$res =~ s/&nbsp;/ /g;
+				$res =~ s/^\s+|\s+$//g;
+
+				if ($fieldspnum == 3) {
+					if ($res =~ /<\/td>/i) {
+						$res =~ s/<\/td>//i;
+					} else {
+						$inpriynyala = 1;
+					}
+				}
+
+				$entry{$fieldsp[$fieldspnum + 8 * $fieldspcoeff]} = $res;
+				$fieldspnum++;
+
+				if ($fieldspnum == 6) {
+					$fieldspnum = -1;
+					$fieldspcoeff++;
+					$fields2priynyala = 0;
+				}
+			}
+		} elsif ($fields2num == 4 and (m'THEAD02.*topTitle"><a href="mailto:(.*)\?subject.*">(.*)</a>'i or 
+				 m'THEAD02.*topTitle"><a href="http://(.*)">(.*)</a>'i)) {
+			my $res = $1;
+			my $res2 = $2;
+
+			if ($fields2num != 4) {
+				die "$f: Unexpected mailto: $_ at $.";
 			}
 
+			$res =~ s/&nbsp;/ /g;
+			$res =~ s/^\s+|\s+$//g;
+			$res2 =~ s/&nbsp;/ /g;
+			$res2 =~ s/^\s+|\s+$//g;
+			if ($res ne $res2) {
+				die "$f: bad mailto format: $_ at $.";
+			}
+
+			$entry{$fields2[$fields2num]} = $res;
+			$fields2num++;
+		} elsif (/THEAD02.*topTitle"><a href="(.*)">(.*)<\/a><\/td>/) {
 			my $res = $1;
+			my $res2 = $2;
+			$res =~ s/&nbsp;/ /g;
+			$res =~ s/^\s+|\s+$//g;
+			$entry{parentref} = $res;
+
+			$res2 =~ s/&nbsp;/ /g;
+			$res2 =~ s/^\s+|\s+$//g;
+			$entry{parentname} = $res2;
+		} elsif ($fields2num == 5 and /THEAD02.*topTitle"><a href="(.*)">(.*)<\/a>/i) {
+			my $res = $1;
+
 			$res =~ s/&nbsp;/ /g;
 			$res =~ s/^\s+|\s+$//g;
 
 			$entry{$fields2[$fields2num]} = $res;
 			$fields2num++;
+		} elsif (/THEAD02/) {
+			die "$f: Unknown thead02 ($fields2num): $_ at $.";
 		}
 	}
 
@@ -222,6 +404,10 @@ for my $f (<${radadir}/*.html>) {
 	}
 
 	for my $f (@fields2) {
+		push @cols, $entry{$f}
+	}
+
+	for my $f (@fieldsp) {
 		push @cols, $entry{$f}
 	}
 
